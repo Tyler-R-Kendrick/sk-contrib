77c93d4410d59ab4ea5e055cd9397321d0be25e6
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2024-06-24 18:41:34 +0000

    Added Regex Plugin

---
+++ src/Plugins/Regex/Plugins.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using Microsoft.SemanticKernel;
+
+namespace SKHelpers.Plugins.Regex;
+
+using Regex = System.Text.RegularExpressions.Regex;
+
+public class CompiledRegexPlugin(Regex compiledRegex)
+{
+    [KernelFunction]
+    [Description("Executes a regex pattern on the input string.")]
+    [return: Description("The match result.")]
+    public MatchCollection ExecuteRegexAsync(
+        [Description("The input string.")]
+        string input)
+        => compiledRegex.Matches(input);
+}
+
+public class RegexPlugin
+{
+    [KernelFunction]
+    [Description("Executes a regex pattern on the input string.")]
+    [return: Description("The match result.")]
+    public MatchCollection ExecuteRegexAsync(
+        [Description("The compiled regex pattern.")]
+        string regex,
+        [Description("The input string.")]
+        string input)
+        => Regex.Matches(regex, input);
+}
