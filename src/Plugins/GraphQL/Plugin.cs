72fe34fad7f82c51731173c86c4a9153eab4303a
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2024-06-24 16:12:44 +0000

    added graphql plugin

---
+++ src/Plugins/GraphQL/Plugin.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using GraphQL;
+using GraphQL.Types;
+using GraphQL.SystemTextJson;
+
+namespace SKHelpers.Plugins.GraphQL;
+
+public class Plugin(Schema schema)
+{
+    [KernelFunction]
+    [Description("Execute a GraphQL query.")]
+    [return: Description("The result of the query.")]
+    public Task<string> QueryAsync(
+        [Description("The graphql query to execute.")] string query,
+        [Description("The root object to execute the query on.")] string root)
+        => schema.ExecuteAsync(_ =>
+        {
+            _.Query = query;
+            _.Root = root;
+        });
+}
