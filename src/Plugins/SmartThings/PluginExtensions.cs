dde4d0c58100e9deb57eb3f31d7f46bc68822b43
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2024-06-24 22:02:41 +0000

    Added SmartThings plugin

---
+++ src/Plugins/SmartThings/PluginExtensions.cs
@@ -0,0 +1,21 @@
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.OpenApi.Extensions;
+
+namespace SKHelpers.Plugins.SmartThings;
+
+public static class DependencyInjectionExtensions
+{
+    public static async Task<Kernel> AddGitHubPluginAsync(
+        this Kernel kernel,
+        ApiManifestPluginParameters apiManifestPluginParameters,
+        CancellationToken cancellationToken = default)
+    {
+        _ = await kernel.ImportPluginFromApiManifestAsync(
+            "smart-things",
+            "SmartThings/apimanifest.json",
+            apiManifestPluginParameters,
+            cancellationToken)
+            .ConfigureAwait(false);
+        return kernel;
+    }
+}
