fb539e693eef1b8e7db09ab43b8ea228ae7a8ef4
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2024-06-25 17:04:33 +0000

    Added mediatr plugin

---
+++ src/Plugins/Messaging/MediatR/Plugin.cs
@@ -0,0 +1,63 @@
+using System.ComponentModel;
+using MediatR;
+using Microsoft.SemanticKernel;
+
+namespace SKHelpers.Plugins.MediatR;
+
+public class Plugin(IMediator mediator,
+    Func<object, string> serialize,
+    Func<string, string, object> deserialize)
+{
+    [KernelFunction]
+    [Description("Sends a request to the mediator.")]
+    [return: Description("The result of the request.")]
+    public async Task<string> SendAsync(
+        [Description("The type of the request.")]
+        string requestType,
+        [Description("The serialized request to send.")]
+        string request,
+        CancellationToken cancellationToken = default)
+    {
+        var requestCLRType = Type.GetType(requestType)
+            ?? throw new ArgumentException($"The type '{requestType}' could not be found.");
+        var requestObj = deserialize(requestType, request)
+            ?? throw new ArgumentException($"The request could not be deserialized to type '{requestCLRType}'.");
+        var result = await mediator.Send(requestObj, cancellationToken)
+            ?? throw new InvalidOperationException("The request returned a null result.");
+        return serialize(result);
+    }
+
+    [KernelFunction]
+    [Description("Publishes a notification to the mediator.")]
+    public async Task PublishAsync(
+        [Description("The type of the notification.")]
+        string notificationType,
+        [Description("The serialized notification to publish.")]
+        string notification,
+        CancellationToken cancellationToken = default)
+    {
+        var notificationCLRType = Type.GetType(notificationType)
+            ?? throw new ArgumentException($"The type '{notificationType}' could not be found.");
+        var notificationObj = deserialize(notificationType, notification)
+            ?? throw new ArgumentException($"The notification could not be deserialized to type '{notificationCLRType}'.");
+        await mediator.Publish(notificationObj, cancellationToken);
+    }
+
+    [KernelFunction]
+    [Description("Creates a stream of responses from the mediator.")]
+    [return: Description("The stream of responses.")]
+    public IAsyncEnumerable<string> CreateStream(
+        [Description("The type of the request.")]
+        string requestType,
+        [Description("The serialized request to send.")]
+        string request,
+        CancellationToken cancellationToken = default)
+    {
+        var requestCLRType = Type.GetType(requestType)
+            ?? throw new ArgumentException($"The type '{requestType}' could not be found.");
+        var requestObj = deserialize(requestType, request)
+            ?? throw new ArgumentException($"The request could not be deserialized to type '{requestCLRType}'.");
+        return mediator.CreateStream(requestObj, cancellationToken)
+            .Select(x => serialize(x ?? throw new InvalidOperationException("The request returned a null result.")));
+    }
+}
